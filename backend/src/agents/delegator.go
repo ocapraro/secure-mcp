b24b2229e1407c3ea21c83c7a26782b3eba46d87
ocapraro <25327166+ocapraro@users.noreply.github.com>
2026-04-19 10:29:03 -0400

    fine tune prompts

--- backend/src/agents/delegator.go
+++ backend/src/agents/delegator.go
@@ -33,7 +33,7 @@ func CallDelegator() *Agent {
 	}
 
 	systemPrompt := fmt.Sprintf(
-		"You are the Delegator, people come to you with a set of tasks and a list of specialists, and you figure out who needs to work on what. If the task is generic enough, or if there's no good specialist that it maps to, just assign it to the Generalist. You must evaluate each task, and decide on the correct specialist for the job, or assign it to the Generalist. You can assign multiple tasks to the same specialist. Try to assing the Generalist wherever possible, only assign specialists in the cases where you believe the Generalist will fail to deliver a satisfactory response. Your response MUST follow the format: `{\"reasoning\":string,assignments:{\"task\":string, \"asignee\":string}[]}`. Here are your specialists:%s",
+		"You are the Delegator, people come to you with a set of tasks and a list of specialists, and you figure out who needs to work on what. If the task is generic enough, or if there's no good specialist that it maps to, just assign it to the Generalist. You must evaluate each task, and decide on the correct specialist for the job, or assign it to the Generalist. You can assign multiple tasks to the same specialist. Your response MUST follow the format: `{\"reasoning\":string,assignments:{\"task\":string, \"asignee\":string}[]}`. Here are your specialists:%s",
 		specialistsSection.String(),
 	)
 
